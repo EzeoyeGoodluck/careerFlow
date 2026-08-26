b60a95250d41363b3b7ec1e902e490181bd7dcdd
Ezeoye Goodluck <116758130+EzeoyeGoodluck@users.noreply.github.com>
2026-08-26 06:59:28 -1200

    Update sign-in page with card component

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -4,22 +4,36 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 
 export default function SignUp() {
-  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   return (
     <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
       <div className="w-full max-w-md border-gray-200 shadow-lg">
         <form className="text-gray-600">
           <div className="space-y-2">
-            <label htmlFor="name" className="text-gray-700">
-              Name
+            <label htmlFor="email" className="text-gray-700">
+              Email
             </label>
             <Input
-              id="name"
-              type="text"
-              placeholder="John Doe"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              id="email"
+              type="email"
+              placeholder="John@Example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="space-y-2">
+            <label htmlFor="password" className="text-gray-700">
+              Password
+            </label>
+            <Input
+              id="password"
+              type="password"
+              placeholder="********"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
