3595e57146bccc6511ef55fe38e39f7667d44132
Ezeoye Goodluck <116758130+EzeoyeGoodluck@users.noreply.github.com>
2026-09-06 03:38:53 -1200

    move image-tabs out of ui folder, it is an app component not a shadcn primitive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import ImageTabs from "@/components/ui/image-tabs";
+import ImageTabs from "@/components/image-tabs";
 import { ArrowRight, Briefcase, CheckCircle2, TrendingUp } from "lucide-react";
 
 import Link from "next/link";
