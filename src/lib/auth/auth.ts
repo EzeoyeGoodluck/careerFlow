a881a5a7f374fe2d2e564cf6ba565998144fd046
Ezeoye Goodluck <116758130+EzeoyeGoodluck@users.noreply.github.com>
2026-09-10 09:33:10 -1200

    creating the auth functionality

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -2,7 +2,31 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 
-export const auth =  betterAuth({
-    database: mongodbAdapter ()
+const client = new MongoClient(process.env.MONGODB_URI!);
+const db = client.db();
 
-})
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client,
+  }),
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 60,
+    }
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+  databaseHooks: {
+    user: {
+      create: {
+        after: async (user) => {
+          if (user.id) {
+            await intializeUseerBoard(user.id);
+          }
+        },
+      },
+    },
+  },
+});
